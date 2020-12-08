488487ee2c1c6a543e6afd6c39d4633057731d0a
raksitnongbua <tan.raksit@gmail.com>
2020-12-08 23:32:25 +0700

    remove unused scripts, add components, add screens

---
+++ src/components/Board.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Board = () => {
+  return <div>this is board</div>;
+};
+
+export default Board;
---
+++ src/screens/Game.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Board from '../components/Board';
+
+const Game = () => {
+  return (
+    <div>
+      <Board />
+    </div>
+  );
+};
+
+export default Game;
